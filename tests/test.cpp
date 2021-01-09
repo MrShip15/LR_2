970fce0e97ac3cb9aa23c654165f846b1b0bc3ff
MrShip15 <61249047+MrShip15@users.noreply.github.com>
2021-01-09 12:35:46 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,16 +1,7 @@
 // Copyright 2020 MrShip15 mr.shi15@gmail.ru
 
 #include <gtest/gtest.h>
-#include <iostream>
-#include "cache_check.hpp"
 
 TEST(Example, EmptyTest) {
-    cache_check a;
-    a.countDirect();
-    std::cout << a;
-    a.countReverse();
-    std::cout << a;
-    a.countRandom();
-    std::cout << a;
     EXPECT_TRUE(true);
 }
