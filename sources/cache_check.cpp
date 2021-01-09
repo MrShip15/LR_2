386c0c69a7571cabb354a4c2749981f787b3ad83
MrShip15 <61249047+MrShip15@users.noreply.github.com>
2021-01-09 10:58:31 +0300

    Update cache_check.cpp

--- sources/cache_check.cpp
+++ sources/cache_check.cpp
@@ -14,57 +14,6 @@ cache_check::cache_check(){
        size <= 3.0/2 * cache_size[2]; size *= 2){
     size_buf.push_back(size);
   }
-  /*
-  for (int i : size_buf) {
-    int* arr = new int[static_cast<int>(i / 4.0 / kylo)];
-    int k = 0;
-    if (travel_variant == "direction") {//Direction method
-      for (size_t j = 0; j < i / 4.0 / kylo; j += sixteen){
-        k = arr[j];
-      }
-      int start_time = clock();
-      for (size_t num = 0; num < ten_hundred; ++num){
-        for (size_t j = 0; j < i / 4.0 / kylo; j += sixteen){
-          k = arr[j];
-        }
-      }
-      int end_time = clock();
-      result_of_experiments.push_back(static_cast<double>(end_time - start_time)
-                                      / CLOCKS_PER_SEC * ten_hundred);
-    } else if (travel_variant == "reverse") {//Reverse method
-      for (size_t j = i / 4.0 / kylo; j > 0 ; j -= sixteen){
-        k = arr[j];
-      }
-      int start_time = clock();
-      for (size_t num = 0; num < 1000; ++num){
-        for (size_t j = i / 4.0 / kylo; j > 0; j -= sixteen){
-          k = arr[j];
-        }
-      }
-      int end_time = clock();
-      result_of_experiments.push_back(static_cast<double>(end_time - start_time)
-                                      / CLOCKS_PER_SEC * ten_hundred);
-    } else {//Random method
-      std::vector<int> index_of_blocks;
-      for (size_t j = 0; j < i / 4.0 / kylo; j += sixteen){
-        index_of_blocks.push_back(j);
-        k = arr[j];
-      }
-      auto rng = std::default_random_engine {};
-      std::shuffle(index_of_blocks.begin(), index_of_blocks.end(), rng);
-
-      int start_time = clock();
-      for (size_t num = 0; num < ten_hundred; ++num){
-        for (auto index : index_of_blocks){
-          k = arr[index];
-        }
-      }
-      int end_time = clock();
-      result_of_experiments.push_back(static_cast<double>(end_time - start_time)
-                                      / CLOCKS_PER_SEC * ten_hundred);
-    }
-  }
-   */
 }
 
 size_t cache_check::getCountBuf() const { return size_buf.size(); }
